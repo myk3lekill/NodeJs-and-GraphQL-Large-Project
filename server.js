344c995cba6a70722eb9df358c05f8af84d3e6d6
me <me@gmail.com>
2023-12-07 12:01:38 +0100

    seveth commit: async resolvers and rooting

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 const express = require('express');
 const path = require('path');
-const {loadFilesSync} = require('@graphql-tools/load-files')
-const {makeExecutableSchema} = require('@graphql-tools/schema');
+const { loadFilesSync } = require('@graphql-tools/load-files')
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { createYoga } = require('graphql-yoga');
+
+const root = {
+  products: require('./products/products.model'),
+  orders: require('./orders/orders.model')
+}
 
 const typesArray = loadFilesSync('**/*', {
   extensions: ['graphql'],
 });
 
 const schema = makeExecutableSchema({
-  typeDefs: typesArray
+  typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: async (parent, args, context, info) => {
+        console.log('Getting the products...');
+        const product = await Promise.resolve(root.products);
+        return product;
+      },
+      orders: async (parent, args, context, info) => {
+        console.log('Getting orders...');
+        const order = await Promise.resolve(root.orders);
+        return order;
+      }
+    }
+  }
 })
 
 const app = express();
+
+//Yoga
+const yoga = createYoga({
+  schema: schema,
+  context: (req) => ({ // Context factory gets called for every request
+      //myToken: req.headers.get('authorization') // I've commented this line because it was causing problems and it seems to work :)
+  }),
+  graphiql: true,
+})
 
 //MiddleWare
 app.use('/graphql', yoga)
